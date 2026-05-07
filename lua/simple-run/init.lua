e06ca43975e416f81fe85e6d5bebb711a60154af
new-mvera <rigmabarsu@proton.me>
2026-05-07 19:48:17 +0300

    init.lua keybind fix

--- lua/simple-run/init.lua
+++ lua/simple-run/init.lua
@@ -5,7 +5,7 @@ local M = {}
 
 function M.setup(opts)
   config.setup(opts)
-  local keymap = config.get().keymap
+  local keymap = config.get().keymap or "<F2>"
   if keymap then
     vim.keymap.set("n", keymap, function()
       M.run()
